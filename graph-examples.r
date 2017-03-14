d1304d6ab6440eb4d7bbdfc9a3e440413f649a94
Michael Sumner <mdsumner@gmail.com>
2017-03-15 06:26:42 +1100

    something

--- graph-examples.r
+++ graph-examples.r
@@ -1,3 +1,30 @@
+library(sf)
+library(scsf)
+nc <- st_read(system.file("shape/nc.shp", package="sf"))
+prim <- PRIMITIVE(minimal_mesh)#[c(9, 15, 24,  5, 16, 31), ])
+library(scgraph)
+library(igraph)
+library(ggraph)
+
+## find vertices that are nodes
+nodes <- sc_node(prim)
+
+## this gets ALL the arc-paths, including duplications in different orders
+split_at <- function(x, vertex) {
+  test <- x$vertex_ %in% vertex
+  if (!any(test)) return(list(x))
+  segs <- c(1, cumsum(abs(diff(test))))
+  split(x, segs)
+}
+library(tidyverse)
+arcs <- bind_rows(unlist(lapply(prim$path_link_vertex %>% split(.$path_), function(x) split_at(x, nodes$vertex_)), recursive = FALSE), .id = "arc")
+
+ggplot(arcs %>% select(-vertex_) %>% left_join(prim$path_link_vertex, "path_") %>% left_join(prim$vertex), 
+       aes(x_, y_, group = arc, colour = arc, lty = arc)) + geom_path() + guides(colour = FALSE)
+
+
+
+
 prim <- sc::PRIMITIVE(nc[c(9, 15, 24,  5, 16, 31), ])
 library(scgraph)
 ig <- scgraph::sc_as_igraph(prim) 
