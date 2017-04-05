52037809dd16499382143307886cf541d37faffa
Michael Sumner <mdsumner@gmail.com>
2017-04-06 00:57:19 +1000

    igraph convert

--- R/scgraph.r
+++ R/scgraph.r
@@ -8,17 +8,31 @@
 #' ## spatstat
 #' ## raw track data
 #' ## make up segments starting with the unjoin thing, that might show the way forward
-#' @importFrom igraph graph_from_data_frame V
+#' @importFrom igraph graph_from_data_frame V as.igraph
 #' @importFrom sc PRIMITIVE
 #' @importFrom dplyr rename
-#' @name sc_as_igraph
+#' @importfrom tidygraph as_tbl_graph
+#' @name as.igraph
 #' @export
-sc_as_igraph <- function(x, layout = TRUE, ...) {
-  prim <- sc::PRIMITIVE(x)
-  g <- igraph::graph_from_data_frame(dplyr::rename_(prim[["segment"]], from = quote(.vertex0), to = quote(.vertex1)))
+#' @export as.igraph
+#' @examples
+#' as.igraph(minimal_mesh)
+as.igraph.PRIMITIVE <- function(x, ..., layout = TRUE) {
+  g <- igraph::graph_from_data_frame(dplyr::rename_(x[["segment"]], from = quote(.vertex0), to = quote(.vertex1)))
   if (layout) {
-    igraph::V(g)$x <- prim$v$x_[match(names(igraph::V(g)), prim$v$vertex_)]
-    igraph::V(g)$y <- prim$v$y_[match(names(igraph::V(g)), prim$v$vertex_)]
+    igraph::V(g)$x <- x$v$x_[match(names(igraph::V(g)), x$v$vertex_)]
+    igraph::V(g)$y <- x$v$y_[match(names(igraph::V(g)), x$v$vertex_)]
   }
   g
 }
+
+#' @export
+#' @name as.igraph
+as.igraph.sf <- function(x, ..., layout = TRUE) {
+  as.igraph(PRIMITIVE(x), ..., layout = layout)
+}
+#' @export 
+#' @name as.igraph
+sc_as_igraph <- as.igraph.PRIMITIVE
+
+
